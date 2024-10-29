96bd6ceb58108bd070d87d053600a36aa407408f
Ramudgar <ramudgary52@gmail.com>
2024-10-29 12:22:39 +0545

    protected route is completed successfully

--- Socialmedia/Frontend/socialmedia/src/App.js
+++ Socialmedia/Frontend/socialmedia/src/App.js
@@ -9,6 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import CategoryComponent from "./Components/CategoryComponent/CategoryComponent";
 import AddPostComponent from "./Components/Post/AddPostComponent";
+import ProtectedRoute from "./Components/protectedRoutes/protectedRoutes";
 
 function App() {
   return (
@@ -19,11 +20,32 @@ function App() {
         <Routes>
           <Route path="/" element={<HomeComponent />} />
           <Route path="/post" element={<PostComponent />} />
-          <Route path="/profile" element={<ProfileComponent />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfileComponent />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/signup" element={<SignupComponent />} />
           <Route path="/login" element={<LoginComponent />} />
-          <Route path='/category' element={<CategoryComponent />} />
-          <Route path="/addpost" element={<AddPostComponent />} />
+          <Route
+            path="/category"
+            element={
+              <ProtectedRoute role="admin">
+                <CategoryComponent />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/addpost"
+            element={
+              <ProtectedRoute>
+                <AddPostComponent />
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </Router>
---
+++ Socialmedia/Frontend/socialmedia/src/Components/protectedRoutes/protectedRoutes.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoute = ({ role, children }) => {
+  const token = localStorage.getItem("token");
+  const authState = {
+    isAuthenticated: token ? true : false,
+    userRole: localStorage.getItem("user")?.userRole,
+  };
+  if (!authState.isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  if (role && authState.userRole !== role) {
+    return <Navigate to="/" />;
+  }
+
+  return children ? children : <Outlet />;
+  // description of outlet
+  // eslint-disable-next-line no-lone-blocks, no-unreachable
+  {
+    /* <Route path="dashboard" element={<ProtectedRoute />} >
+  <Route path="analytics" element={<Analytics />} />
+  <Route path="reports" element={<Reports />} />
+  <Route path="settings" element={<Settings />} />
+</Route> */
+  }
+};
+
+export default ProtectedRoute;
